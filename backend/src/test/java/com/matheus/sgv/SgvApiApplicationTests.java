29162e5dc5e886c54b8327c120a7643eece3d2a3
pc <matheuss.monteiro9@gmail.com>
2026-07-22 15:53:50 -0300

    correção em SgvApiApplicationTest

--- backend/src/test/java/com/matheus/sgv/SgvApiApplicationTests.java
+++ backend/src/test/java/com/matheus/sgv/SgvApiApplicationTests.java
@@ -2,12 +2,12 @@ package com.matheus.sgv;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SgvApiApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+	void contextLoads() {}
 }

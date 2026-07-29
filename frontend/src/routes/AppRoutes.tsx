9bd9378032c81902863510cec992f27b71609b37
pc <matheuss.monteiro9@gmail.com>
2026-07-29 16:04:22 -0300

    roteia telas principais

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -2,8 +2,10 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../pages/auth/LoginPage'
 import { RegistroPage } from '../pages/auth/RegistroPage'
 import { VerificarEmailPage } from '../pages/auth/VerificarEmailPage'
-import { RegistrarViagemPage } from '../pages/viagens/RegistrarViagemPage'
-import { DashboardPage } from '../pages/DashboardPage'
+import { CalendarioPage } from '../pages/CalendarioPage'
+import { HistoricoPage } from '../pages/HistoricoPage'
+import { FinanceiroPage } from '../pages/FinanceiroPage'
+import { AppLayout } from '../components/layout/AppLayout'
 import { ProtectedRoute } from './ProtectedRoute'
 
 export function AppRoutes() {
@@ -12,17 +14,41 @@ export function AppRoutes() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/cadastro" element={<RegistroPage />} />
       <Route path="/verificar-email" element={<VerificarEmailPage />} />
-      <Route path="/" element={
-        <ProtectedRoute>
-        <DashboardPage />
-        </ProtectedRoute>
-      }/>
+
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <AppLayout>
+              <CalendarioPage />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/historico"
+        element={
+          <ProtectedRoute>
+            <AppLayout>
+              <HistoricoPage />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      />
+
+      <Route
+        path="/financeiro"
+        element={
+          <ProtectedRoute>
+            <AppLayout>
+              <FinanceiroPage />
+            </AppLayout>
+          </ProtectedRoute>
+        }
+      />
+
       <Route path="*" element={<Navigate to="/" replace />} />
-      <Route path="/viagens/nova" element={
-        <ProtectedRoute>
-        <RegistrarViagemPage />
-        </ProtectedRoute>
-      }/>
     </Routes>
   )
 }
